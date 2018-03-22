e26f22b08eeb7c0b1c3173c75b750607ea6a9a9a
Sebastian Kissling <=>
2018-03-22 12:27:34 -0300

    Add project files to test Lottie in React Native

---
+++ App.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  View,
+  Animated,
+  Easing
+} from 'react-native';
+import LottieView from 'lottie-react-native';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      progress: new Animated.Value(0),
+    };
+  }
+
+  componentDidMount() {
+    Animated.sequence([
+      Animated.timing(this.state.progress, {
+        toValue: 1,
+        duration: 4000,
+        easing: Easing.linear,
+      }),
+      Animated.timing(this.state.progress, {
+        toValue: 0,
+        duration: 0
+      })
+    ]).start(this.componentDidMount.bind(this));
+  }
+
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <LottieView source={require('./animations/gears.json')} progress={this.state.progress} />
+      </View>
+    );
+  }
+}
